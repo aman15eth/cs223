122f3f34634764b091de9496133170fb3879579a
aman15eth <102879608+aman15eth@users.noreply.github.com>
2022-07-08 02:51:31 -0700

    assignment 3

---
+++ Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace labexec
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.NUMBER = new System.Windows.Forms.TextBox();
+            this.SKU = new System.Windows.Forms.TextBox();
+            this.DATE = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.ITEMNAME = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.QUANTITY = new System.Windows.Forms.TextBox();
+            this.PRICE = new System.Windows.Forms.TextBox();
+            this.ADD = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // NUMBER
+            // 
+            this.NUMBER.Location = new System.Drawing.Point(65, 94);
+            this.NUMBER.Name = "NUMBER";
+            this.NUMBER.Size = new System.Drawing.Size(140, 20);
+            this.NUMBER.TabIndex = 0;
+            this.NUMBER.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // SKU
+            // 
+            this.SKU.Location = new System.Drawing.Point(543, 94);
+            this.SKU.Name = "SKU";
+            this.SKU.Size = new System.Drawing.Size(146, 20);
+            this.SKU.TabIndex = 1;
+            this.SKU.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // DATE
+            // 
+            this.DATE.Location = new System.Drawing.Point(297, 94);
+            this.DATE.Name = "DATE";
+            this.DATE.Size = new System.Drawing.Size(146, 20);
+            this.DATE.TabIndex = 2;
+            this.DATE.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(101, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "NUMBER";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(344, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "DATE";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(603, 40);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "SKU";
+            // 
+            // ITEMNAME
+            // 
+            this.ITEMNAME.Location = new System.Drawing.Point(77, 168);
+            this.ITEMNAME.Name = "ITEMNAME";
+            this.ITEMNAME.Size = new System.Drawing.Size(645, 20);
+            this.ITEMNAME.TabIndex = 6;
+            this.ITEMNAME.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(100, 138);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "ITEM NAME";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(100, 236);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(62, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "QUANTITY";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(469, 236);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(39, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "PRICE";
+            // 
+            // QUANTITY
+            // 
+            this.QUANTITY.Location = new System.Drawing.Point(65, 264);
+            this.QUANTITY.Name = "QUANTITY";
+            this.QUANTITY.Size = new System.Drawing.Size(118, 20);
+            this.QUANTITY.TabIndex = 10;
+            this.QUANTITY.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // PRICE
+            // 
+            this.PRICE.Location = new System.Drawing.Point(438, 264);
+            this.PRICE.Name = "PRICE";
+            this.PRICE.Size = new System.Drawing.Size(100, 20);
+            this.PRICE.TabIndex = 11;
+            this.PRICE.TextChanged += new System.EventHandler(this.textBox6_TextChanged);
+            // 
+            // ADD
+            // 
+            this.ADD.Location = new System.Drawing.Point(108, 322);
+            this.ADD.Name = "ADD";
+            this.ADD.Size = new System.Drawing.Size(75, 23);
+            this.ADD.TabIndex = 12;
+            this.ADD.Text = "ADD";
+            this.ADD.UseVisualStyleBackColor = true;
+            this.ADD.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(463, 322);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "CANCEL";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.ADD);
+            this.Controls.Add(this.PRICE);
+            this.Controls.Add(this.QUANTITY);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.ITEMNAME);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.DATE);
+            this.Controls.Add(this.SKU);
+            this.Controls.Add(this.NUMBER);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox NUMBER;
+        private System.Windows.Forms.TextBox SKU;
+        private System.Windows.Forms.TextBox DATE;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox ITEMNAME;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox QUANTITY;
+        private System.Windows.Forms.TextBox PRICE;
+        private System.Windows.Forms.Button ADD;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,92 @@
+﻿using labexec.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace labexec
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+     s   {
+
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ITEM i = new ITEM();
+
+            i.NUMBER = Convert.ToInt32(NUMBER.Text);
+            i.DATE = DATE.Text;
+            i.SKU = SKU.Text;
+            i.item_name = ITEMNAME.Text;
+            i.QUANTITY = Convert.ToInt32(QUANTITY.Text);
+            i.price = Convert.ToDouble(PRICE.Text);
+
+
+
+
+            MessageBox.Show("record saved sussesfully");
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            NUMBER.Text = null;
+            DATE.Text = " ";
+            SKU.Text = " ";
+            ITEMNAME.Text = " ";
+            QUANTITY.Text = " ";
+            PRICE.Text = null;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+
+        }
+    }
+}
